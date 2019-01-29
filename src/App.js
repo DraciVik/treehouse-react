24a202dd0d9e91b20f296c0ee8cf2ed00826c2d9
DraciVik <viktor.stojanov90@gmail.com>
2019-01-29 19:25:03 +0100

    Extracted a component into its own and fix rendering issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const desc = "I just learned stuff";
-const myTitleID = "main-title";
-const name = "Guil";
+const Header = () => {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+};
 
-const header = (
-  <header>
-    <h1 id={myTitleID}>{name}'s First React Element!</h1>
-    <p>{desc}</p>
-  </header>
-);
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Vikt</span>
+      <Counter />
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* Players List */}
+      <Player />
+    </div>
+  );
+};
 
 export default App;
