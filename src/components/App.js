61df27ad96be5998936496d2585c63862d50b334
DraciVik <viktor.stojanov90@gmail.com>
2019-01-31 04:05:27 +0100

    Moved state up from Counter to App

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,23 @@ class App extends Component {
       players: [
         {
           name: "Viktor",
-          id: 1
+          id: 1,
+          score: 0
         },
         {
           name: "Slagjana",
-          id: 2
+          id: 2,
+          score: 0
         },
         {
           name: "Nikola",
-          id: 3
+          id: 3,
+          score: 0
         },
         {
           name: "Borce",
-          id: 4
+          id: 4,
+          score: 0
         }
       ]
     };
@@ -33,16 +37,30 @@ class App extends Component {
       };
     });
   };
+  //   If it's not an arrow function the "this" method references increment score.
+  // When it's an arrow function like here this belongs to the parent component - the Counter class
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => {
+      // prevState is used so that the state is updated before using the callback and modify it
+      return {
+        // setState doesn't guarantee that state will be updated at once. Thats why we use prevState
+        score: (prevState.players[index].score += delta)
+      };
+    });
+  };
   render() {
     return (
       <div className="scoreboard">
         <Header title="Scoreboard" totalPlayers={this.state.players.length} />
 
         {/* Players List */}
-        {this.state.players.map(player => (
+        {this.state.players.map((player, index) => (
           <Player
+            changeScore={this.handleScoreChange}
+            score={player.score}
             name={player.name}
             id={player.id}
+            index={index}
             key={player.id.toString()}
             removePlayer={this.handleRemovePlayer}
           />
